7a0ab22d5739eab7920755cbf79c7e210f7fed0e
Abhisingh980 <kr.abhinesh147@gmail.com>
2024-01-27 00:08:49 +0530

    weather api through streamlit and happy.csv data visualization bug fix

--- weather_front_api.py
+++ weather_front_api.py
@@ -15,14 +15,14 @@ if place:
     data, date, sky_dict = bw.get_detail(place, frequency)
     if date:
         if option in "Temperature":
-            temperature = [data[index]['temp']for index in range(len(data))]
+            temperature = [data[index]['temp'] for index in range(len(data))]
             temperature = [temperature[i] / 10 for i in range(len(data))]
             figure = px.line(x=date, y=temperature, labels={"x": "Date", "y": "Temperature (C)"})
             st.plotly_chart(figure)
         else:
-            sky_type = [sky_dict[index][0]['main']for index in range(len(data))]
-            sky_description = [sky_dict[index][0]['description']for index in range(len(data))]
-            image_name = {"Clouds": "image/cloudy.jpg","Rain": "image/rain.jpg",
+            sky_type = [sky_dict[index][0]['main'] for index in range(len(data))]
+            sky_description = [sky_dict[index][0]['description'] for index in range(len(data))]
+            image_name = {"Clouds": "image/cloudy.jpg", "Rain": "image/rain.jpg",
                           "Snow": "image/snow.jpeg", "Clear": "image/clear.png"}
             sky = []
             for image in sky_type:
@@ -30,5 +30,6 @@ if place:
 
             st.image(sky, caption=sky_description, width=200)
     else:
-        st.write(data)
+        st.write(data.title())
+
 
